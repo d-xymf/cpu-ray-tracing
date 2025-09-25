5cea2f7fdb43e49c56da06486e6b91e2ce31b12b
d-xymf <david.x.schimpf@gmail.com>
2025-09-25 14:19:11 +0200

    ray tracing sphere with simple diffuse lighting

---
+++ HitInfo.py
@@ -0,0 +1,5 @@
+class HitInfo:
+    def __init__(self, hit, coords, normal):
+        self.hit = hit
+        self.coords = coords
+        self.normal = normal
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import HitInfo
 
 # the render result will be saved here
 image = Image.new(mode="RGB", size=(400, 300))
@@ -14,21 +15,35 @@ sphere_radius = 1.0
 # always pointing in +z direction
 cam_vfov = 40.0 * 3.14159265/180.0
 near = 1.0/np.tan(cam_vfov)
+# define a point light source at (-1, 3, 3)
+light_pos = np.array([-1.0,3.0,2.0])
 
 # c - center of the sphere
 # r - radius of the sphere
 # rd - ray direction
-def ray_sphere_intersection(c, r, rd):
+def ray_sphere_intersection(sc: np.array, sr: float, rd: np.array) -> HitInfo.HitInfo:
 
-    k = np.dot(c, rd)
-    # discard if sphere is behind us
-    if k < 0:
-        return False
+    # coefficients for quadratic equation
+    a = np.dot(rd, rd)
+    b = -2*np.dot(rd, sc)
+    c = np.dot(sc, sc) - sr*sr
 
-    if np.linalg.norm(k*rd - c) <= r:
-        return True
+    # discard if sphere is behind us or we are inside the sphere
+    if b > 0 or c < 0:
+        return HitInfo.HitInfo(False, None, None)
+    
+    # check for hit
+    d = b*b - 4*a*c # discriminant
+    if d < 0:
+        return HitInfo.HitInfo(False, None, None)# no hit
     else:
-        return False
+        k = (-b - np.sqrt(d)) / (2*a) # this scalar multiplied by rd gives coords of intersection
+
+        intersection = rd * k
+
+        normal = (k*rd - sc) / np.linalg.norm(k*rd - sc)
+
+        return HitInfo.HitInfo(True, intersection, normal)
 
 # fragment shader
 def frag(frag_coord: tuple[int, int]) -> tuple[int, int, int]:
@@ -41,12 +56,22 @@ def frag(frag_coord: tuple[int, int]) -> tuple[int, int, int]:
 
     # create a ray for this pixel
     ray_dir = np.append(uv, near)
+    # ray direction must be normalized 
+    # for sphere intersection calculation to work
     ray_dir = ray_dir / np.linalg.norm(ray_dir)
 
     # if ray intersects sphere make pixel white else black
     color = np.array([0.0, 0.0, 0.0])
-    if ray_sphere_intersection(sphere_center, sphere_radius, ray_dir):
-        color = np.array([1.0, 1.0, 1.0])
+    ray_hit_info = ray_sphere_intersection(sphere_center, sphere_radius, ray_dir)
+    if ray_hit_info.hit:
+
+        light_dir = light_pos - ray_hit_info.coords
+        light_dir = light_dir / np.linalg.norm(light_dir)
+
+        normal = ray_hit_info.normal
+
+        # simple diffuse lighting
+        color = np.array([1.0, 1.0, 1.0]) * np.dot(light_dir, normal)
 
     # convert np array to tuple of ints (0.0-1.0 -> 0-255)
     int_color = np.rint(color * 255).astype(int)
